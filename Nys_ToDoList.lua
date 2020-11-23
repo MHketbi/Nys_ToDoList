a1948aaa0fae397c11b6f03f3d1ad73635ab2f48
Mubarak Harran Alketbi <mubarak.harran@gmail.com>
2020-11-24 02:06:10 +0400

    Add files via upload

---
+++ Nys_ToDoList.lua
@@ -0,0 +1,267 @@
+
+NysToDoListDB = {
+	["global"] = {
+		["tuto_progression"] = 6,
+	},
+	["profileKeys"] = {
+	},
+	["profiles"] = {
+		["Default"] = {
+			["itemsList"] = {
+				["Covenant"] = {
+					["Daily calls"] = {
+						["tabName"] = "Daily",
+						["checked"] = false,
+						["description"] = "Emissary like daily calls for rewards",
+					},
+					["Adventures"] = {
+						["tabName"] = "Daily",
+						["checked"] = false,
+						["description"] = "do your table missions",
+					},
+					["Treasures"] = {
+						["tabName"] = "Daily",
+						["checked"] = false,
+						["description"] = "For Reservoir Anima and other items",
+					},
+					["Rares"] = {
+						["tabName"] = "Daily",
+						["checked"] = false,
+						["description"] = "For Reservoir Anima and other items",
+					},
+					["Reservoir Anima"] = {
+						["tabName"] = "Daily",
+						["checked"] = false,
+						["description"] = "From Dungeons and WQ",
+					},
+				},
+				["Quests"] = {
+					["pvp weekly 2"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["heroic dungeon weekly"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["pvp weekly 1"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["normal dungeon weekly"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+				},
+				["Torghast"] = {
+					["Wing 1"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Wing 2"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+				},
+				["Legendary/Conduit"] = {
+					["Legendary Recipe"] = {
+						["tabName"] = "Daily",
+						["checked"] = false,
+						["description"] = "know where the recipe you want comes from and farm it",
+					},
+					["Farm Conduit"] = {
+						["tabName"] = "Daily",
+						["checked"] = false,
+						["description"] = "know where your best conduit come from and farm that content",
+					},
+				},
+				["The Vault"] = {
+					["M+ (1 time)"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["M+ (10 times)"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Rated-PVP (1250 Honor)"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Raid (3 bosses)"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Rated-PVP (6250 Honor)"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["M+ (4 times)"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Raid (10 bosses)"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Rated-PVP (2500 Honor)"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Raid (7 bosses)"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+				},
+				["Mandatory"] = {
+					["Complete Campaing"] = {
+						["tabName"] = "Daily",
+						["checked"] = false,
+					},
+					["Complete Covenant quest"] = {
+						["tabName"] = "Daily",
+						["checked"] = false,
+					},
+				},
+				["Mythic0"] = {
+					["Plaguefall"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Necrotic Wake"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Halls of Atonement"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Theater of Pain"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Mists of Tirna Scithe"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Sanguine Depths"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["Spires of Ascension"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+					["De Other Side"] = {
+						["tabName"] = "Weekly",
+						["checked"] = false,
+					},
+				},
+				["Maw"] = {
+					["Reputation"] = {
+						["tabName"] = "Daily",
+						["checked"] = false,
+						["description"] = "to open more Torghast upgrades",
+					},
+					["Stygia"] = {
+						["tabName"] = "Daily",
+						["checked"] = false,
+						["description"] = "Currency for socket and Torghast upgrades",
+					},
+				},
+			},
+			["lastLoadedTab"] = "ToDoListUIFrameTab1",
+			["frameSize"] = {
+				["width"] = 340.0000610351563,
+			},
+			["autoReset"] = {
+				["Weekly"] = 1606280399,
+				["Daily"] = 1606193999,
+			},
+			["undoTable"] = {
+				{
+					["tabName"] = "Daily",
+					["itemName"] = "Wing 1",
+					["checked"] = false,
+					["catName"] = "Torghast",
+				}, -- [1]
+				{
+					["tabName"] = "Daily",
+					["itemName"] = "Wing 2",
+					["checked"] = false,
+					["catName"] = "Torghast",
+				}, -- [2]
+				{
+					["tabName"] = "Daily",
+					["itemName"] = "Farm Anima",
+					["checked"] = false,
+					["catName"] = "Covenant",
+				}, -- [3]
+				{
+					["tabName"] = "Daily",
+					["itemName"] = "Farm Rares",
+					["checked"] = false,
+					["catName"] = "Covenant",
+				}, -- [4]
+				{
+					["tabName"] = "Daily",
+					["itemName"] = "Farm Treasures",
+					["checked"] = false,
+					["catName"] = "Covenant",
+				}, -- [5]
+				{
+					["tabName"] = "Weekly",
+					["itemName"] = "2x M+ Dungeon",
+					["checked"] = false,
+					["catName"] = "the Vault",
+				}, -- [6]
+				{
+					["tabName"] = "Weekly",
+					["itemName"] = "15x M+ Dungeon",
+					["checked"] = false,
+					["catName"] = "the Vault",
+				}, -- [7]
+				{
+					["tabName"] = "Weekly",
+					["itemName"] = "10x M+ Dungeon",
+					["checked"] = false,
+					["catName"] = "the Vault",
+				}, -- [8]
+				{
+					["tabName"] = "Weekly",
+					["itemName"] = "1x M+ Dungeon",
+					["checked"] = false,
+					["catName"] = "the Vault",
+				}, -- [9]
+				{
+					["tabName"] = "Weekly",
+					["itemName"] = "3x Raid bosses",
+					["checked"] = false,
+					["catName"] = "the Vault",
+				}, -- [10]
+				{
+					["tabName"] = "Weekly",
+					["itemName"] = "4x M+ Dungeon",
+					["checked"] = false,
+					["catName"] = "the Vault",
+				}, -- [11]
+				{
+					["tabName"] = "Weekly",
+					["itemName"] = "7x Raid bosses",
+					["checked"] = false,
+					["catName"] = "the Vault",
+				}, -- [12]
+				{
+					["tabName"] = "Weekly",
+					["itemName"] = "Mists of Tirna Scite",
+					["checked"] = false,
+					["catName"] = "Mythic0",
+				}, -- [13]
+			},
+			["framePos"] = {
+				["xOffset"] = 206.2225952148438,
+				["yOffset"] = 33.18515777587891,
+			},
+		},
+	},
+}
